505643149f29e85dd0c975a9f2aa9e43e704d9ca
Rajangane <pratiksharajangane7@gmail.com>
2021-12-05 14:08:10 +0530

    [Pratiksha] ADD : Stock Account Management

--- Object_Oriented_Programs/Program.cs
+++ Object_Oriented_Programs/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("************* Object Oriented Programs *****************");
+//Console.WriteLine("************* Object Oriented Programs *****************");
+
+using Object_Oriented_Programs;
+
+Console.Write("Select Number:\n1)Stock\n");
+int option = Convert.ToInt32(Console.ReadLine());
+switch (option)
+{
+    case 1:
+        new Stock().add();
+        break;
+    default:
+        Console.WriteLine("invalid choice");
+        break;
+}
---
+++ Object_Oriented_Programs/Stock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programs
+{
+    public class Stock
+    {
+        //instance variable declaration
+        public int totalshare = 0;
+        public int totalPrice = 0;
+
+        public void add() //creating method
+        {
+            Console.WriteLine("Enter Number Of Stocks:-");
+            int NumberOfStocks = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= NumberOfStocks; i++) //for loop till number of stocks
+            {
+                Console.WriteLine("Enter ShareName:-");
+                string ShareName = Console.ReadLine();
+
+                Console.WriteLine("Enter Total Number Of Shares:-");
+                int NoOfShares = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Price Per Share:-");
+                int PricePerEachShare = int.Parse(Console.ReadLine());
+
+                Stockvalue(NoOfShares, PricePerEachShare, ShareName);//calling stockvalue()
+            }
+        }
+        public void Stockvalue(int p, int q, string r)//creating method
+        {
+            int stockPrice = p * q; //calculating stock price
+            Console.WriteLine("The Price for stock " + r + " shares is :  " + stockPrice);
+            totalPrice += (p * q);
+            Console.WriteLine("the total price of the stock is  " + totalPrice);
+        }
+    }
+}
